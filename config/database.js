ff08eef461938b64a90a5fbfc8f2740d04d1fccc
NeusaVerbeno <verbenoneusamaria@gmail.com>
2025-04-25 21:19:15 -0300

    Node Neusa

---
+++ config/database.js
@@ -0,0 +1,20 @@
+import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const connection = await mysql.createConnection ({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
+    
+});
+
+try {
+    await connection.execute ('select 1')
+    console.log ('conexão Estabelecida')
+} catch (erro) {
+    console.log ('Erro ao se conectar com o banco de dados', erro);
+};
+
+export default connection;
